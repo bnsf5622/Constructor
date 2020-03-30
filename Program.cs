46c60ed09ce7e923f42b72cffdec8850681c2d0f
Chuck <bnsf5622@gmail.com>
2020-03-30 15:21:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace OOPSConcept
+{
+    class ClassRoom
+    {
+        string text = "I have apples each costing RS";
+        public ClassRoom(int i)
+        {
+            Console.WriteLine(text +  + i);
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            ClassRoom newObj = new ClassRoom(10);
+            Console.ReadLine();
+
+        }
+    }
+}
